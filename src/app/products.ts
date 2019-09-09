16ad0332565c5d53d9e30aacc4b263c2d4f0894b
Ammar Mohrat <ammar-mohrat@hotmail.com>
2019-09-08 20:37:37 -0400

    V1

---
+++ src/app/products.ts
@@ -0,0 +1,24 @@
+export const products = [
+  {
+    name: 'Cheese Fries',
+    price: 5.99,
+    description: 'Fries smothered in Cheese and bacon'
+  },
+  {
+    name: 'Onion Rings',
+    price: 4.99,
+    description: 'Original Onion Rings coats in our signature batter.'
+  },
+  {
+    name: 'Grilled Chicken Alfredo',
+    price: 11.99,
+    description: 'Delisious pasta smothered in our house made alfredo sauce topped with grilled chicken'
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
